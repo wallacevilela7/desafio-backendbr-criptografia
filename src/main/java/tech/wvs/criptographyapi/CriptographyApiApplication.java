5a51c82d45ae537d497d63a0f49a2dcdcaa764b9
wallacevilela7 <wallace.vilela9@gmail.com>
2025-04-15 14:46:31 -0300

    Create spring app

---
+++ src/main/java/tech/wvs/criptographyapi/CriptographyApiApplication.java
@@ -0,0 +1,13 @@
+package tech.wvs.criptographyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriptographyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CriptographyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/wvs/criptographyapi/CriptographyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.wvs.criptographyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriptographyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
